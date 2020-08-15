c89e9e42e2d9fdf05610cf23f9ec049741046ecd
abditimer <thatimer@gmail.com>
2020-08-15 09:41:17 +0100

    added stories feature

--- lib/widgets/profile_avatar.dart
+++ lib/widgets/profile_avatar.dart
@@ -5,38 +5,46 @@ import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 class ProfileAvatar extends StatelessWidget {
   final String imageUrl;
   final bool isActive;
+  final bool hasBorder;
 
   const ProfileAvatar({
     Key key,
     @required this.imageUrl,
     this.isActive = false,
+    this.hasBorder = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
+        // Wrap a ca in a ca so that we see a green circle behind avatar
         CircleAvatar(
           radius: 20.0,
-          backgroundColor: Colors.grey[200],
-          backgroundImage: CachedNetworkImageProvider(imageUrl),
+          backgroundColor: Palette.facebookBlue,
+          child: CircleAvatar(
+            radius: hasBorder ? 17.0 : 20.0,
+            backgroundColor: Colors.grey[200],
+            backgroundImage: CachedNetworkImageProvider(imageUrl),
+          ),
         ),
         // if isActive == True, return Positioned, else skip
-        isActive ? Positioned(
-          bottom: 0.0,
-          right: 0.0,
-          child: Container(
-              height: 15.0,
-              width: 15.0,
-              decoration: BoxDecoration(
-                  color: Palette.online,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    width: 2.0,
-                    color: Colors.white,
-                  ))),
-        ) 
-        : const SizedBox.shrink()
+        isActive
+            ? Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                    height: 15.0,
+                    width: 15.0,
+                    decoration: BoxDecoration(
+                        color: Palette.online,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 2.0,
+                          color: Colors.white,
+                        ))),
+              )
+            : const SizedBox.shrink()
       ],
     );
   }
