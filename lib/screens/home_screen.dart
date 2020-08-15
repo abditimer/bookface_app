ba07dc02ca21109c78bfd7086a19c03823a19ad9
abditimer <thatimer@gmail.com>
2020-08-15 12:47:30 +0100

    post feature added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -56,6 +57,14 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final Post post = posts[index];
+              return PostContainer(post: post);
+            },
+            childCount: posts.length,
+          ))
         ],
       ),
     );
